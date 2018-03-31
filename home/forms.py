6bf0dfa683e51e3373d8f8945ddef0445f90cd49
Sai Kiran <iam191911918114@gmail.com>
2018-03-31 08:13:08 +0530

    second commit

--- home/forms.py
+++ home/forms.py
@@ -5,5 +5,4 @@ class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
-        
         fields = ['username', 'email', 'password']
